cf58350ad6a5cb400e6e4a043436f4a88ffff231
naveen <neonew8984@gmail.com>
2021-05-17 21:56:05 +0530

    fetch api and asyn and await to get the info

--- javascript/script.js
+++ javascript/script.js
@@ -3,4 +3,13 @@ const button = document.getElementById('btn');
 
 button.addEventListener('click', displayRecipe);
 
-function displayRecipe() {}
+function displayRecipe() {
+    const res = await fetch(
+        'https://www.themealdb.com/api/json/v1/1/random.php'
+    );
+
+    const data = await res.json();
+
+    const code = data.meals[0].strYoutube.substring(32);
+    const recipe = data.meals[0].strInstructions;
+}
