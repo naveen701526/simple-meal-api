be8a7805b1dc7427da92e6609c6f6722ad25b400
naveen <neonew8984@gmail.com>
2021-05-17 21:53:36 +0530

    referring to essential elements in dom

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+const mealApi = document.getElementById('meal-api');
+const button = document.getElementById('btn');
+
+button.addEventListener('click', displayRecipe);
+
+function displayRecipe() {}
