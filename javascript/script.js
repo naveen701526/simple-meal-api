701b0fee8ea900b5ad34938f8f8adea060038da9
naveen <neonew8984@gmail.com>
2021-05-17 21:56:46 +0530

    display the json info in the dom

--- javascript/script.js
+++ javascript/script.js
@@ -12,4 +12,20 @@ function displayRecipe() {
 
     const code = data.meals[0].strYoutube.substring(32);
     const recipe = data.meals[0].strInstructions;
+
+    mealApi.innerHTML = `
+        <h2>${data.meals[0].strMeal}</h2>
+        <p>${recipe.replaceAll('\n', '<br/><br/>')}</p>
+        <div class="iframe-container">
+        <iframe
+        width='560'
+        height='315'
+        src='https://www.youtube.com/embed/${code}'
+        title='YouTube video player'
+        frameborder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowfullscreen></iframe>;
+        </iframe>
+        </div>
+    `;
 }
